2107279febf0ec817ad8628365619a58a3e9d879
VitalySlabada <vitaly.slabada@gmail.com>
2022-03-29 17:39:12 -0500

    Added basic MVC structure

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,24 +1,43 @@
 package com.cydeo.controller;
 
-
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping ("/user")
+@RequestMapping("/user")
 public class UserController {
 
+    RoleService roleService;
+    UserService userService;
 
-    @GetMapping("/create")
-    public String userCreate (Model model){
-        model.addAttribute("user", new UserDTO());
-//        model.addAttribute("roles", )
-
-        return ("/user/create");
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
     }
 
+    @GetMapping("/create")
+    public String createUser(Model model){
+
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute ("user") UserDTO user)  {
+
+        userService.save(user);
+
+        return "redirect:/user/create";
+    }
 }
